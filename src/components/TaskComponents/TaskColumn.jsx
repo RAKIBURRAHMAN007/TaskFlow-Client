911f3f972c443fe18943b7c598c84d7465648d8a
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-02-21 00:26:03 +0600

    texte update

--- src/components/TaskComponents/TaskColumn.jsx
+++ src/components/TaskComponents/TaskColumn.jsx
@@ -40,7 +40,7 @@ export const Task = ({ task, refetch }) => {
   const onDelete = (id) => {
     Swal.fire({
       title: "Are you sure?",
-      text: "You won't be able to revert this user!",
+      text: "You won't be able to revert this Task!",
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
